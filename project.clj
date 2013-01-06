f950e578612333431b8f34c2ae8881ae38715718
Gereon Frey <me@gereonfrey.de>
2013-01-06 19:53:06 +0100

    cleaned up the fresh project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject beth "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure[Script] WebApplication Development Framework"
+  :url "https://github.com/gfrey/beth"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
--- test/beth/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns beth.core-test
-  (:use clojure.test
-        beth.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
