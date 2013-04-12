e01c5d289c6f7ed8bb90d0b9e1ce110a3e74c33f
Gereon Frey <me@gereonfrey.de>
2013-04-12 21:31:41 +0200

    updated libraries

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/gfrey/beth"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[aleph                       "0.3.0-beta14"]
+  :dependencies [[aleph                       "0.3.0-beta16"]
                  [com.cemerick/piggieback     "0.0.4"]
                  [enlive                      "1.1.1"]
                  [log4j/log4j                 "1.2.16"
@@ -13,12 +13,12 @@
                                                com.sun.jmx/jmxri]]
                  [midje                       "1.5.1"]
                  [net.cgrand/moustache        "1.2.0-alpha2"]
-                 [org.clojure/clojure         "1.5.0"]
-                 [org.clojure/clojurescript   "0.0-1552"]
+                 [org.clojure/clojure         "1.5.1"]
+                 [org.clojure/clojurescript   "0.0-1586"]
                  [org.clojure/data.json       "0.2.1"]
                  [org.clojure/google-closure-library-third-party "0.0-2029"]
                  [org.clojure/tools.logging   "0.2.6"]
-                 [org.clojure/tools.namespace "0.2.2"]
+                 [org.clojure/tools.namespace "0.2.3"]
                  [ring/ring-core              "1.2.0-beta2"]]
   :main beth.clj.core
   :repl-options {:init-ns beth.clj.repl
